79e24eac052f80a912270dbd3ad5c881f8da24c4
Soroush Khosravi <sorush.kh68@gmail.com>
2019-03-16 14:45:10 +0000

    We can delete a food from a restaurent's menue.

--- lib/restaurent.rb
+++ lib/restaurent.rb
@@ -3,4 +3,12 @@ class Restaurent
   def initialize(list = {})
     @list = list
   end
+
+  def add(food,price)
+    @list[food] = price
+  end
+
+  def deleting(food)
+    @list.delete(food)
+  end
 end
--- spec/restaurent_spec.rb
+++ spec/restaurent_spec.rb
@@ -8,4 +8,16 @@ describe Restaurent do
     a = {}
     expect(@restaurent.list).to eq a
   end
+  it "We can add a food with its price to the menue" do
+    @restaurent.add("green dish","20")
+    list = {"green dish"=>"20"}
+    expect(@restaurent.list).to eq list
+  end
+
+  it "We can delete a food from menue" do
+    @restaurent.add("green dish","20")
+    @restaurent.deleting("green dish")
+    a= {}
+    expect(@restaurent.list).to eq a
+  end
 end
